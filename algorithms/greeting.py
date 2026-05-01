824719b938278e2ef055a06a3267bd0941083419
Hrishikesh Desai <Hrishikeshdesai05@gmail.com>
2026-04-30 20:10:50 -0400

    add greeting algorithm

---
+++ algorithms/greeting.py
@@ -0,0 +1,9 @@
+#Task: Write a script that asks for a user's name and their favourite color and then prints a message like 
+# "Hello, [Name]! I see your favourite color is [Color]"
+
+
+name = input("What is your name: ")
+
+color = input("What is your favourite color: ")
+
+print(f"Hello, {name}! I see your favourite color is {color}.")
